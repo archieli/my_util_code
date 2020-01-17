7fe2d3bbde63db431dfe61733e50bab758c8dee5
archieli <44295793+archieli@users.noreply.github.com>
2020-01-17 13:13:45 +0800

    Update ntohll.c

--- ntohll.c
+++ ntohll.c
@@ -1,10 +1,10 @@
 uint64_t ntohll(const uint64_t &ullIn)
 {
-    char*   pCur    = (char*)(&ullIn);
+    char* pVal = (char*)(&ullIn);
     if (1 != ntohs(1))  {
-        uint64_t  ullOut  = (((uint64_t)(ntohl(*(uint32_t*)pCur))) 
+        uint64_t  ullOut  = (((uint64_t)(ntohl(*(uint32_t*)pVal))) 
                                        << (sizeof(uint32_t) * 8))
-                                       + ntohl(*(uint32_t*)(pCur + sizeof(uint32_t)));
+                                       + ntohl(*(uint32_t*)(pVal + sizeof(uint32_t)));
         return ullOut;
     }   else    {
         return ullIn;         
